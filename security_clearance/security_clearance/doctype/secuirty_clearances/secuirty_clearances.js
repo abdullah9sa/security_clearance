58cfcc09576bf5f7cc836c1e175431d981edff7d
Abdullah Salih <abdullahalsalih@gmail.com>
2025-05-26 12:03:29 +0300

    Changes 26-5

---
+++ security_clearance/security_clearance/doctype/secuirty_clearances/secuirty_clearances.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Telafer University and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Secuirty Clearances", {
+// 	refresh(frm) {
+
+// 	},
+// });
